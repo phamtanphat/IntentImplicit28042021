d564d2b06b0b29d429ced39a58bbc28607734d05
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-26 20:30:56 +0700

    kiểm tra va duyệt request

--- app/src/main/java/com/example/intentimplicit28042021/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit28042021/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.intentimplicit28042021;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.intentimplicit28042021.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mBinding;
+    int REQUEST_CODE_CAMERA = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Check permission granted
+                if (ActivityCompat.checkSelfPermission(MainActivity.this , Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    // Xin quyền
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CODE_CAMERA
+                            );
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_CAMERA){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Chấp thuận sử dụng quyền dùng camera", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Không chấp thuận dùng camera", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
